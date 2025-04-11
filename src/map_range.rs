f1f4e521c57d8b85528400e0928a21585ee784b9
jplatnerFTI <jonathan.platner@faithtechinc.com>
2025-04-10 22:52:36 -0700

    #comment Refactor map_range_clamped method to accept values by value instead of references

--- src/map_range.rs
+++ src/map_range.rs
@@ -13,11 +13,11 @@ pub trait MapRange: Sized + PartialEq + Sub<Output = Self> + Div<Output = Self> 
         let t = (*self - in_min) / (in_max - in_min);
         Ok(t.lerp_unclamped(out_min, out_max))
     }
-    fn map_range_clamped(&self, in_min: &Self, in_max: &Self, out_min: &Self, out_max: &Self) -> Result<Self, ScalarError> {
+    fn map_range_clamped(&self, in_min: Self, in_max: Self, out_min: Self, out_max: Self) -> Result<Self, ScalarError> {
         if in_min == in_max {
             return Err(ScalarError::InvalidRangeError(format!("Input range cannot be zero")));
         }
-        let t = (*self - *in_min) / (*in_max - *in_min);
-        Ok(t.lerp_clamped(*out_min, *out_max))
+        let t = (*self - in_min) / (in_max - in_min);
+        Ok(t.lerp_clamped(out_min, out_max))
     }
 }
